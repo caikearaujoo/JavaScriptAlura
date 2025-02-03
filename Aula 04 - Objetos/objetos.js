eea9d4b98633059836b9f06088b7aa8e1d93f150
Caike Araújo <127609797+caikearaujoo@users.noreply.github.com>
2025-02-03 18:13:20 -0300

    Add files via upload

---
+++ Aula 04 - Objetos/objetos.js
@@ -0,0 +1,10 @@
+const listCpf = ['42181390835', '2431880953'];
+
+const infosPessoa = ['nome', 'José Silva', 'idade', 32];
+
+const objPessoa = {
+    nome: 'José Silva',
+    idade: 32,
+    cpf: '42181390835',
+    turma: 'Java Script'
+}
